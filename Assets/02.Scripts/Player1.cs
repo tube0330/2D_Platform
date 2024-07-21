7dbf9575b23be94ac478ec1ad575189db0a1ea28
tube0330 <zien0516@naver.com>
2024-07-22 00:42:56 +0900

    240722_error: different speed between character

--- Assets/02.Scripts/Player1.cs
+++ Assets/02.Scripts/Player1.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player1 : MonoBehaviour
 {
     [SerializeField] SpriteRenderer spriteRenderer;
-    [SerializeField] float speed = 5f;
+    float speed = 5f;
     private Rigidbody2D rb;
     private Animator ani;
     private Transform tr;
@@ -19,7 +19,7 @@ public class Player1 : MonoBehaviour
 
     void FixedUpdate()
     {
-        float h = 0f;
+        h = 0f;
 
         if (Input.GetKey(KeyCode.A))
             h = -1f;
@@ -40,7 +40,7 @@ public class Player1 : MonoBehaviour
         // 애니메이션 설정
         if (Mathf.Abs(h) > 0)
             ani.SetBool("isWalk", true);
-            
+
         else
             ani.SetBool("isWalk", false);
     }
--- Assets/02.Scripts/Player2.cs
+++ Assets/02.Scripts/Player2.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player2 : MonoBehaviour
 {
     [SerializeField] SpriteRenderer spriteRenderer;
-    [SerializeField] float speed = 5f;
+    float speed = 5f;
     private Rigidbody2D rb;
     private Animator ani;
     private Transform tr;
@@ -20,7 +20,7 @@ public class Player2 : MonoBehaviour
 
      void FixedUpdate()
     {
-        float h = 0f;
+        h = 0f;
 
         if (Input.GetKey(KeyCode.LeftArrow))
             h = -1f;
@@ -40,7 +40,7 @@ public class Player2 : MonoBehaviour
         // 애니메이션 설정
         if (Mathf.Abs(h) > 0)
             ani.SetBool("isWalk", true);
-            
+
         else
             ani.SetBool("isWalk", false);
     }
